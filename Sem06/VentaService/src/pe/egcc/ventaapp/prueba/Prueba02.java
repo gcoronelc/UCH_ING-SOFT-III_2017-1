9a4e170a18c10876e62c1da3441a31750f7d00d7
302-MODULO <302-MODULO>
2017-05-04 16:20:15 -0500

    Sem 06

---
+++ Sem06/VentaService/src/pe/egcc/ventaapp/prueba/Prueba02.java
@@ -0,0 +1,37 @@
+package pe.egcc.ventaapp.prueba;
+
+import java.util.List;
+import pe.egcc.ventaapp.model.ResumenModel;
+import pe.egcc.ventaapp.model.VentaModel;
+import pe.egcc.ventaapp.service.impl.VentaServiceMemo;
+
+public class Prueba02 {
+
+  public static void main(String[] args) {
+    // Servicio
+    VentaServiceMemo service = new VentaServiceMemo();
+    // Agregar ventas
+    List<String> cate = service.getCategorias();
+    service.regVenta(new VentaModel(cate.get(0), "Pedro", "Algo", 345.87, 4));
+    service.regVenta(new VentaModel(cate.get(1), "Pedro", "Sdddd", 456.23, 7));
+    service.regVenta(new VentaModel(cate.get(2), "Pedro", "Eeeee", 123.45, 3));
+    service.regVenta(new VentaModel(cate.get(1), "Pedro", "Aalgo", 986.12, 8));
+    service.regVenta(new VentaModel(cate.get(01), "Pedro", "Aaaaa", 765.78, 9));
+    // Listado
+    System.out.println("LISTADO");
+    System.out.println("----------------------------------------");
+    List<VentaModel> ventas = service.listadoVentas(cate.get(1));
+    for (VentaModel venta : ventas) {
+      System.out.println(venta.toString());
+    }
+    
+    // Resumen
+    System.out.println("RESUMEN");
+    System.out.println("------------------------------------");
+    for(ResumenModel model: service.obtenerResumen()){
+      System.out.println(model.toString());
+    }
+
+  }
+
+}
---
+++ Sem06/VentaService/src/pe/egcc/ventaapp/service/impl/VentaServiceMemo.java
@@ -0,0 +1,90 @@
+package pe.egcc.ventaapp.service.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import pe.egcc.ventaapp.model.ResumenModel;
+import pe.egcc.ventaapp.model.VentaModel;
+import pe.egcc.ventaapp.service.spec.VentaServiceSpec;
+import pe.egcc.ventaapp.util.MateUtil;
+
+public class VentaServiceMemo implements VentaServiceSpec{
+
+  @Override
+  public List<String> getCategorias() {
+    String[] categorias = {
+        Data.LAPTOPS,    
+        Data.CELULARES,
+        Data.TABLE,
+        Data.TELEVISORES,
+        Data.IMPRESORAS,
+        Data.ACCESORIOS};
+    List<String> lista = Arrays.asList(categorias);
+    return lista;
+  }
+
+  @Override
+  public VentaModel calcVenta(VentaModel venta) {
+    // Varibles    
+    double subtotal, impuesto, total;
+    // Proceso
+    total = MateUtil.roundTo2Dec(venta.getPrecio() * venta.getCantidad());
+    subtotal = MateUtil.roundTo2Dec(total / 1.18);
+    impuesto = MateUtil.roundTo2Dec(total - subtotal);
+    // Reporte
+    venta.setSubtotal(subtotal);
+    venta.setImpuesto(impuesto);
+    venta.setTotal(total);
+    return venta;
+  }
+
+  @Override
+  public void regVenta(VentaModel venta) {
+    venta = calcVenta(venta);
+    Data.data.add(venta);
+  }
+
+  @Override
+  public List<VentaModel> listadoVentas(String categoria) {
+    List<VentaModel> listaAux = new ArrayList<>();
+    for (VentaModel venta : Data.data) {
+      if(venta.getCategoria().equals(categoria)){
+        listaAux.add(venta);
+      }
+    }
+    return listaAux;
+  }
+
+  @Override
+  public List<ResumenModel> obtenerResumen() {
+    List<ResumenModel> listaResumen = new ArrayList<>();
+    // ------------------------------------------------------------
+    List<String> categorias = getCategorias();
+    for (String categoria : categorias) {
+      ResumenModel resumen = obtenerResumenCategoria(categoria);
+      if( resumen.getCantidad() > 0 ){
+      listaResumen.add(resumen);
+      }
+    }
+    // ------------------------------------------------------------
+    return listaResumen;
+  }
+
+  private ResumenModel obtenerResumenCategoria(String categoria) {
+    ResumenModel rm = new ResumenModel();
+    rm.setCategoria(categoria);
+    for(VentaModel ventaModel: Data.data){
+      if(ventaModel.getCategoria().equals(categoria)){
+      rm.setCantidad(rm.getCantidad() + ventaModel.getCantidad());
+      rm.setImporte(rm.getImporte()+ ventaModel.getSubtotal());
+      rm.setImpuesto(rm.getImpuesto()+ ventaModel.getImpuesto());
+      rm.setTotal(rm.getTotal()+ ventaModel.getTotal());
+      }
+    }
+    return rm;
+  }
+  
+  
+  
+  
+}
